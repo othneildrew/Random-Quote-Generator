eb13c190b5b2f817d334cfe36b74c4f2081452c0
Othneil Drew <codeguydrew@gmail.com>
2019-01-24 22:01:37 -0600

    Add random background color, add 3 new quotes, increase auto timer to 7 seconds

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ Treehouse Techdegree:
 FSJS project 1 - A Random Quote Generator
 ******************************************/
 
-// Create array of quotes
+let autoTimer;
 let quotes = [
   {
     quote: 'Life is 10% what happens to me and 90% of how I react to it.',
@@ -43,21 +43,33 @@ let quotes = [
     quote: 'Only greatness pays, forget average.',
     source: 'Othneil Drew',
     year: 2018,
-    category: 'movies'
+    category: 'success'
+  },
+  {
+    quote: 'Everyday is a bank account, and time is our currency. No one is rich, no one is poor, we\'ve got 24 hours each.',
+    source: 'Christopher Rice',
+    category: 'life'
+  },
+  {
+    quote: 'Nearly all men can stand adversity, but if you want to test a man\'s character, give him power.',
+    source: 'Abraham Lincoln',
+    category: 'leadership'
+  },
+  {
+    quote: 'Money can buy you a fine dog, but only love can make him wag his tail.',
+    source: 'Kinky Friedman',
+    category: 'money'
   }
 ];
 
-let autoTimer;
 
+function generateRandomNo(maxNo) {
+  return Math.floor(Math.random() * Math.floor(maxNo));
+}
 
 
 // Generator random number then return a quote object
 function getRandomQuote() {
-  // Generate a random number
-  function generateRandomNo(maxNo) {
-    return Math.floor(Math.random() * Math.floor(maxNo));
-  }
-
   let randomNumber = generateRandomNo(quotes.length);
   return quotes[randomNumber];
 }
@@ -69,10 +81,25 @@ function clearAutoQuote() {
 }
 
 
-// Create a function that changes the quote automatically after 5 seconds
+// Create a function that changes the quote automatically after 7 seconds
 function autoChangeQuote() {
   clearAutoQuote();
-  autoTimer = window.setTimeout(printQuote, 5000);
+  autoTimer = window.setTimeout(printQuote, 7000);
+}
+
+
+// Generate a random color and set the background
+function generateRandomColor() {
+  let colorArray = Array();
+
+  for(i = 0; i < 3; i++) {
+    let value = generateRandomNo(255);
+    colorArray.push(value);
+  }
+
+  let colorValue = colorArray.join();
+
+  document.getElementsByTagName('BODY')[0].style.backgroundColor = 'rgb('+ colorValue +')';
 }
 
 
@@ -92,9 +119,10 @@ function printQuote() {
 
   document.getElementById('quote-box').innerHTML = quoteBox;
 
+  generateRandomColor();
+
   autoChangeQuote();
 }
-
 
 
 // On "Show another quote" button click, print a new random quote
